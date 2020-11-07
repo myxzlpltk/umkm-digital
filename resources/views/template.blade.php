9070ff4a04f1abba0b9861ff97e01c8c0a6ab57c
Saddam Azy <myxzlpltk@gmail.com>
2020-11-07 20:37:48 +0700

    add template view

---
+++ resources/views/template.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default.app')
+
+@section('title', 'Selamat Datang')
+@section('body.className', 'bg-white')
+
+@push('stylesheets')
+@endpush
+
+@section('header')
+@endsection
+
+@section('content')
+@endsection
+
+@push('scripts')
+@endpush
