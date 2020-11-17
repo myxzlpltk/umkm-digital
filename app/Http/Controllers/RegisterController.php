58d4c154472d57fa4c003cd0b4f903a5f3b255fd
Saddam Azy <myxzlpltk@gmail.com>
2020-11-17 15:00:40 +0700

    save avatar google

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Socialite\Facades\Socialite;
 
 class RegisterController extends Controller{
@@ -33,15 +35,22 @@ class RegisterController extends Controller{
                 ]);
             }
 
+            $urlAvatar = preg_replace('/s[\d]+/', 's500', $googleUser->avatar);
+            $contentAvatar = Http::get($urlAvatar)->body();
+            $fileAvatar = $googleUser->getId().'.jpg';
+            Storage::put('avatars/'.$fileAvatar, $contentAvatar);
+
             $user = new User;
             $user->name = $googleUser->getName();
             $user->email = $googleUser->getEmail();
             $user->password = '';
+            $user->avatar = $fileAvatar;
             $user->google_name = $googleUser->getName();
             $user->google_email = $googleUser->getEmail();
             $user->google_avatar = $googleUser->getAvatar();
             $user->role = Session::pull('register_role');
             $user->markEmailAsVerified();
+
             $user->save();
 
             auth()->login($user);
