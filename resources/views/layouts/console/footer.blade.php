75741584e4c29499d0e59b44a262571f6f4d0ebb
Saddam Azy <myxzlpltk@gmail.com>
2020-11-23 09:02:04 +0700

    Remove footer menu

--- resources/views/layouts/console/footer.blade.php
+++ resources/views/layouts/console/footer.blade.php
@@ -2,16 +2,13 @@
     <div class="row align-items-center justify-content-lg-between">
         <div class="col-lg-6">
             <div class="copyright text-center text-lg-left text-muted">
-                &copy; 2020 <a href="#" class="font-weight-bold ml-1" target="_blank">UMKM</a>
+                &copy; 2020 <a href="{{ route('home') }}" class="font-weight-bold ml-1">{{ config('app.name') }}</a>
             </div>
         </div>
         <div class="col-lg-6">
             <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                 <li class="nav-item">
-                    <a href="#" class="nav-link" target="_blank">Mitra</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link" target="_blank">Tentang Kami</a>
+                    <a href="#" class="nav-link">Tentang Kami</a>
                 </li>
             </ul>
         </div>
--- resources/views/layouts/default/footer.blade.php
+++ resources/views/layouts/default/footer.blade.php
@@ -3,16 +3,13 @@
         <div class="row align-items-center justify-content-xl-between">
             <div class="col-xl-6">
                 <div class="copyright text-center text-xl-left text-muted">
-                    &copy; 2020 <a href="{{ url('/')  }}" class="font-weight-bold ml-1" target="_blank">{{ config('app.name', 'UMKM Digital') }}</a>
+                    &copy; 2020 <a href="{{ route('home') }}" class="font-weight-bold ml-1">{{ config('app.name') }}</a>
                 </div>
             </div>
             <div class="col-xl-6">
                 <ul class="nav nav-footer justify-content-center justify-content-xl-end">
                     <li class="nav-item">
-                        <a href="{{ url('about-us') }}" class="nav-link" target="_blank">Tentang Kami</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ url('license') }}" class="nav-link" target="_blank">Lisensi</a>
+                        <a href="#" class="nav-link">Tentang Kami</a>
                     </li>
                 </ul>
             </div>
