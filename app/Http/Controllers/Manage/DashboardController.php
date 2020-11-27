687a4c8eb3776d65eadff6319e5d890d72f12930
Saddam Azy <myxzlpltk@gmail.com>
2020-11-27 22:40:44 +0700

    update dashboard

--- app/Http/Controllers/Manage/DashboardController.php
+++ app/Http/Controllers/Manage/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Manage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -12,11 +15,28 @@ class DashboardController extends Controller{
 
     public function index(){
         if(Gate::allows('isAdmin')){
-            return view('manage.admin');
+            return view('manage.admin', [
+                'stat_product' => Product::count(),
+                'stat_category' => Category::count(),
+                'stat_transaction' => Order::count(),
+                'stat_canceled' => Order::whereIn('status_code', [
+                    Order::CANCELED,
+                    Order::REFUND_BEING_PROCESSED,
+                    Order::REFUND_COMPLETED,
+                ])->count()
+            ]);
         }
         elseif(Gate::allows('isSeller')){
             return view('manage.seller', [
                 'seller' => auth()->user()->seller,
+                'stat_product' => auth()->user()->seller->products()->count(),
+                'stat_category' => auth()->user()->seller->categories()->count(),
+                'stat_transaction' => auth()->user()->seller->orders()->count(),
+                'stat_canceled' => auth()->user()->seller->orders()->whereIn('status_code', [
+                    Order::CANCELED,
+                    Order::REFUND_BEING_PROCESSED,
+                    Order::REFUND_COMPLETED,
+                ])->count()
             ]);
         }
 
--- resources/views/manage/admin.blade.php
+++ resources/views/manage/admin.blade.php
@@ -15,19 +15,15 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Trafik Pengunjung</h5>
-                            <span class="h2 font-weight-bold mb-0">350,897</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Produk</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $stat_product }}</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                                <i class="ni ni-active-40"></i>
+                                <i class="ni ni-box-2"></i>
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Sejak bulan lalu</span>
-                    </p>
                 </div>
             </div>
         </div>
@@ -37,19 +33,15 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Pelanggan baru</h5>
-                            <span class="h2 font-weight-bold mb-0">2,356</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Kategori</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $stat_category }}</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
-                                <i class="ni ni-chart-pie-35"></i>
+                                <i class="ni ni-tag"></i>
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Sejak bulan lalu</span>
-                    </p>
                 </div>
             </div>
         </div>
@@ -59,8 +51,8 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Penjualan</h5>
-                            <span class="h2 font-weight-bold mb-0">924</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Transaksi</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $stat_transaction }}</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
@@ -68,10 +60,6 @@
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Sejak bulan lalu</span>
-                    </p>
                 </div>
             </div>
         </div>
@@ -81,19 +69,15 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Kualitas Transaksi</h5>
-                            <span class="h2 font-weight-bold mb-0">49,65%</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Transaksi Gagal</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $stat_canceled }}</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
-                                <i class="ni ni-chart-bar-32"></i>
+                                <i class="fa fa-exclamation-triangle"></i>
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Sejak bulan lalu</span>
-                    </p>
                 </div>
             </div>
         </div>
@@ -104,61 +88,6 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-xl-8">
-            <div class="card bg-default">
-                <div class="card-header bg-transparent">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <h6 class="text-light text-uppercase ls-1 mb-1">Kilasan</h6>
-                            <h5 class="h3 text-white mb-0">Nilai penjualan</h5>
-                        </div>
-                        <div class="col">
-                            <ul class="nav nav-pills justify-content-end">
-                                <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#chart-sales-dark" data-update='{"data":{"datasets":[{"data":[0, 20, 10, 30, 15, 40, 20, 60, 60]}]}}' data-prefix="$" data-suffix="k">
-                                    <a href="#" class="nav-link py-2 px-3 active" data-toggle="tab">
-                                        <span class="d-none d-md-block">Bulan</span>
-                                        <span class="d-md-none">B</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item" data-toggle="chart" data-target="#chart-sales-dark" data-update='{"data":{"datasets":[{"data":[0, 20, 5, 25, 10, 30, 15, 40, 40]}]}}' data-prefix="$" data-suffix="k">
-                                    <a href="#" class="nav-link py-2 px-3" data-toggle="tab">
-                                        <span class="d-none d-md-block">Minggu</span>
-                                        <span class="d-md-none">M</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Chart -->
-                    <div class="chart">
-                        <!-- Chart wrapper -->
-                        <canvas id="chart-sales-dark" class="chart-canvas"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4">
-            <div class="card">
-                <div class="card-header bg-transparent">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <h6 class="text-uppercase text-muted ls-1 mb-1">Kualitas Transaksi</h6>
-                            <h5 class="h3 mb-0">Persentase</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Chart -->
-                    <div class="chart">
-                        <canvas id="chart-bars" class="chart-canvas"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 
 @push('scripts')
